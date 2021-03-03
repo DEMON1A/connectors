596538548bbc1177b501001b80afb99438c6c4a8
Mohamed Dief <mdaif1332@gmail.com>
2021-03-03 07:52:13 +0200

    Fixing ACE On Sighting.

--- sighting/src/sighting.py
+++ sighting/src/sighting.py
@@ -9,7 +9,7 @@ class SightingConnector:
         # Instantiate the connector helper from config
         config_file_path = os.path.dirname(os.path.abspath(__file__)) + "/config.yml"
         config = (
-            yaml.load(open(config_file_path), Loader=yaml.FullLoader)
+            yaml.load(open(config_file_path), Loader=yaml.SafeLoader)
             if os.path.isfile(config_file_path)
             else {}
         )
