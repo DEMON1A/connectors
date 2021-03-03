51d46dfa0ab5aae5ec415414fca36fbf73334494
Mohamed Dief <mdaif1332@gmail.com>
2021-03-03 08:06:41 +0200

    Fixing ACE On taxii2

--- taxii2/src/taxii2.py
+++ taxii2/src/taxii2.py
@@ -20,7 +20,7 @@ class Taxii2Connector:
         config_file_path = os.path.dirname(os.path.abspath(__file__))
         config_file_path += "/config.yml"
         config = (
-            yaml.load(open(config_file_path), Loader=yaml.FullLoader)
+            yaml.load(open(config_file_path), Loader=yaml.SafeLoader)
             if os.path.isfile(config_file_path)
             else {}
         )
