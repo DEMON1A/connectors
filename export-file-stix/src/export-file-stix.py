0972f7f51773f1c6c98531ee1ba5e0094900006b
Mohamed Dief <mdaif1332@gmail.com>
2021-03-03 08:05:57 +0200

    Fixing ACE On export-file-stix

--- export-file-stix/src/export-file-stix.py
+++ export-file-stix/src/export-file-stix.py
@@ -11,7 +11,7 @@ class ExportFileStix:
         # Instantiate the connector helper from config
         config_file_path = os.path.dirname(os.path.abspath(__file__)) + "/config.yml"
         config = (
-            yaml.load(open(config_file_path), Loader=yaml.FullLoader)
+            yaml.load(open(config_file_path), Loader=yaml.SafeLoader)
             if os.path.isfile(config_file_path)
             else {}
         )
